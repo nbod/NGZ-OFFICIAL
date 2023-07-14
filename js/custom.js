d35f6ed1fe9264f1377da667139bd8480473f3ea
nbod <nyaradzochimudzura1999@gmail.com>
2023-07-14 14:31:21 +0200

    navigation-mobile

--- js/custom.js
+++ js/custom.js
@@ -39,6 +39,32 @@ for (var i = 0; i < accordions.length; i++) {
 }
 
 
+var mobileMenuBtn = document.querySelector("#mobile-menu-btn");
+var mobileMenu = document.querySelector(".mobile-menu");
+mobileMenuBtn.addEventListener("click", () => {
+  if (mobileMenu.style.display === "none") {
+    mobileMenu.style.display = "flex";
+    mobileMenuBtn.innerHTML = "Close";
+  } 
+  else {
+    mobileMenu.style.display = "none";
+    mobileMenuBtn.innerHTML = "Menu";
+  }
+});
+var mobileMenuBtn = document.querySelector("#mobile-menu-btn");
+var mobileMenu = document.querySelector(".mobile-menu");
+mobileMenuBtn.addEventListener("click", () => {
+  if (mobileMenu.style.display === "none") {
+    mobileMenu.style.display = "flex";
+    mobileMenuBtn.innerHTML = "Close";
+  } 
+  else {
+    mobileMenu.style.display = "none";
+    mobileMenuBtn.innerHTML = "Menu";
+  }
+});
+
+
     // HOME SLIDER & COURSES & CLIENTS
     $('.home-slider').owlCarousel({
       animateOut: 'fadeOut',
