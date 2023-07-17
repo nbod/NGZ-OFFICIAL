44401b1c58f760eacffa029d77fdfdb1ed1ad600
nbod <nyaradzochimudzura1999@gmail.com>
2023-07-17 11:00:16 +0200

    js-mobile

--- js/main.js
+++ js/main.js
@@ -99,6 +99,24 @@ for (var i = 0; i < accordions.length; i++) {
   }
 }
 
+$('.dropdown').click(function(e) {
+    e.preventDefault();
+    setTimeout($.proxy(function() {
+      if ('ontouchstart' in document.documentElement) {
+        $(this).siblings('.dropdown-backdrop').off().remove();
+      }
+    }, this), 0);
+  });
+
+  $('.dropdown-menu').click(function(e) {
+    e.preventDefault();
+    setTimeout($.proxy(function() {
+      if ('ontouchstart' in document.documentElement) {
+        $(this).siblings('.dropdown-backdrop').off().remove();
+      }
+    }, this), 0);
+  });
+
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
